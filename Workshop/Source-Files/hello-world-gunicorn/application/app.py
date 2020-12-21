1de0569d41ddeb26fbd03726ec08d26ac2d4a9c1
Jim Barlow <jim@jimbarlow.com>
2020-12-20 19:27:09 -0500

    s2i in openshift

---
+++ Workshop/Source-Files/hello-world-gunicorn/application/app.py
@@ -0,0 +1,26 @@
+import sys
+import os
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+application = Flask(__name__)
+api = Api(application)
+
+class Greeting (Resource):
+    def get(self):
+        return 'Hello Another World from a generic Universal Base Image optimized for RHEL! try hitting this resource at /python_version'
+
+class PythonVersion (Resource):
+    def get(self):
+        return (sys.version)
+
+class ListFiles (Resource):
+    def get(self):
+        return (os.listdir('/')) 
+
+api.add_resource(Greeting, '/')
+api.add_resource(PythonVersion, '/python_version')
+api.add_resource(ListFiles, '/directory')
+
+if __name__ == '__main__':
+    application.run('0.0.0.0','8080')
